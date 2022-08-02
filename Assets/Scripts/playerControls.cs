5c6c11a12175b985d964e6accc8370be7926fcb4
espadianto2000 <20170532@aloe.ulima.edu.pe>
2022-08-01 20:48:34 -0500

    network jump

--- Assets/Scripts/playerControls.cs
+++ Assets/Scripts/playerControls.cs
@@ -14,14 +14,22 @@ public class playerControls : NetworkBehaviour
     [SerializeField]
     private NetworkVariable<float> HoriAxis = new NetworkVariable<float>();
 
+    [SerializeField]
+    private NetworkVariable<bool> flagJump = new NetworkVariable<bool>();
+
+    
+
     //client caching
     private float oldVertAxis;
     private float oldHoriAxis;
+    public bool flagGround = false;
+    
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         no = GetComponent<NetworkObject>();
+        flagJump.Value = false;
     }
     void FixedUpdate()
     {
@@ -37,6 +45,10 @@ public class playerControls : NetworkBehaviour
     private void UpdateServer()
     {
         rb.MovePosition(new Vector3(transform.position.x + (HoriAxis.Value*0.05f), transform.position.y, transform.position.z + (vertAxis.Value*0.05f)));
+        if (flagJump.Value)
+        {
+            jump();
+        }
     }
     private void UpdateClient()
     {
@@ -48,6 +60,12 @@ public class playerControls : NetworkBehaviour
             oldVertAxis = vert;
         }
         updateClientPositionServerRpc(hori, vert);
+        if(Input.GetKey(KeyCode.Space) && flagGround)
+        {
+            flagGround = false;
+            jumpServerRpc();
+        }
+
     }
     [ServerRpc]
     private void updateClientPositionServerRpc(float h, float v)
@@ -55,4 +73,25 @@ public class playerControls : NetworkBehaviour
         HoriAxis.Value = h;
         vertAxis.Value = v;
     }
+
+    [ServerRpc]
+    private void jumpServerRpc()
+    {
+        jump();
+    }
+    
+
+    private void jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 7.5f, rb.velocity.z);
+        //flagJump.Value = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("suelo"))
+        {
+            flagGround = true;
+        }
+    }
+
 }
